6ddb9e3e5855b29fe9ad8e0921c4dcec77488f43
mqtent <mqtent@gmail.com>
2019-06-12 09:06:03 +0700

    React router example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 import List from './List';
+import Blog from './Blog';
 
 const App = () => {
-  return (<div>
-    <List/>
-  </div>)
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/blog">Blog</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Route exact path="/" component={List} />
+        <Route path="/blog" component={Blog} />
+      </div>
+    </Router>
+  )
 }
  
 export default App;
---
+++ src/Blog.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Blog = (props) => {
+    return <div>
+        <h3>Blog Title</h3>
+        <p>Blog Content...</p>
+    </div>
+}
+
+export default Blog;
